b3496237834ca3d93b9e7ebadecb1c5e938e0159
WangNing <34965243@qq.com>
2023-07-07 21:34:55 +0800

    登录状态判断

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -12,8 +12,8 @@ const sleep = (time) => {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-const getLoginBtn = async (page) => {
-    return await (page.$$eval('a', links => links.filter(link => link.textContent === '登录/注册'))[0])
+const getLoginUser = async (page) => {
+    return await page.$('.login-user')
 }
 
 const launchBrowser = async (headless) => {
@@ -30,11 +30,10 @@ const launchLoginPage = async (page) => {
     const loginPage = await browser.newPage();
     await loginPage.goto(buffUrl);
     await sleep(1000)
-    let loginBtn = await getLoginBtn(loginPage)
-    while (loginBtn != null && loginBtn != undefined) {
-        await sleep(1000)
-        loginBtn = await getLoginBtn(loginPage)
-        console.log(loginBtn)
+    let loginBtn = await getLoginUser(loginPage)
+    while (loginBtn == null || loginBtn == undefined) {
+        await sleep(3000)
+        loginBtn = await getLoginUser(loginPage)
     }
     await sleep(1000)
     await browser.close()
@@ -78,37 +77,47 @@ const gotoBuffMarketPage = async (page, pageNum, needReload = false) => {
     }
 }
 
+const gotoBuffHomePage = async (page) => {
+    const buffUrl = 'https://buff.163.com/';
+    console.log('Goto Buff Home Page')
+    await page.goto(buffUrl)
+}
+
 
 (async () => {
     let pageNum = 0
     let browser = await launchBrowser(true);
     let page = await browser.newPage();
-
-    // 监听页面内的所有网络响应
-    page.on('response', async response => {
-        if (/api\/market\/goods/.exec(response.url())) {
-            const res = JSON.parse(await response.text());
-            for (const item of res.data.items) {
-                processBuffItem(item)
-                await sleep(5000);
-                console.log('=======================')
-            }
-            setTimeout(() => {
-                gotoBuffMarketPage(page, ++pageNum, true)
-            }, 5000)
-        }
-    });
-
-    await gotoBuffMarketPage(page, pageNum);
-
-    await sleep(1000);
-    const loginBtn = await getLoginBtn(page);
-    if (loginBtn != null && loginBtn != undefined) {
+    await gotoBuffHomePage(page)
+    await sleep(3000);
+    const loginBtn = await getLoginUser(page);
+    console.log(loginBtn)
+    if (loginBtn == null || loginBtn == undefined) {
         await sleep(1000);
         await browser.close();
         await launchLoginPage();
         return;
+    } else {
+        // 监听页面内的所有网络响应
+        page.on('response', async response => {
+            if (/api\/market\/goods/.exec(response.url())) {
+                const res = JSON.parse(await response.text());
+                for (const item of res.data.items) {
+                    processBuffItem(item)
+                    await sleep(5000);
+                    console.log('=======================')
+                }
+                setTimeout(() => {
+                    gotoBuffMarketPage(page, ++pageNum, true)
+                }, 5000)
+            }
+        });
+        gotoBuffMarketPage(page, pageNum);
     }
+
+
+
+
     // Save a screenshot of the results.
     // await page.screenshot({ path: 'headless-test-result.png' });
 })();
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+                processBuffItem(item)
