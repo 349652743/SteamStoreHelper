c26cb63771cee4057ce0b7747babcb187f904bc7
WangNing <34965243@qq.com>
2023-07-07 19:35:18 +0800

    谷歌表格api鉴权

--- src/google-sheet.js
+++ src/google-sheet.js
@@ -1,13 +1,13 @@
 const { google } = require('googleapis');
-const { OAuth2Client } = require('google-auth-library');
+const credentials = require('../credentials.json');
 
-
-const oAuth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET);
+const auth = new google.auth.GoogleAuth({
+  credentials: credentials,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
 
 (async () => {
-    const accessToken = await oAuth2Client.getAccessToken();
-
-    const sheets = google.sheets({ version: 'v4', auth: accessToken });
+    const sheets = google.sheets({ version: 'v4', auth: auth });
     const spreadsheetId = '1TX7Y16n7DsPaTojMb_c3LVNp2Hgwtt4EvIrWJIHaWPA';
     const range = 'Sheet1!A1:C3';
     const response = await sheets.spreadsheets.values.get({ spreadsheetId, range });
